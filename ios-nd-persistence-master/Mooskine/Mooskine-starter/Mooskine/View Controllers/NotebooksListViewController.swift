fe05f64041167ffd739e6269fff19bad3f43fb9e
Suresh N <forsuresh@yandex.com>
2019-02-25 18:30:36 -0500

    BugFix: TableView not updating when entries are added/deleted on notebookController: 	* Added the following: 		controllerWillChangeContent 		controllerDidChangeContent 		controller(_ controller: NSFetchedResultsController<NSFetchRequestResult> ... newIndexPath: IndexPath?)
    			These are the delegate functions that fire when changes are made to coreData with FetchResultsController

--- ios-nd-persistence-master/Mooskine/Mooskine-starter/Mooskine/View Controllers/NotebooksListViewController.swift
+++ ios-nd-persistence-master/Mooskine/Mooskine-starter/Mooskine/View Controllers/NotebooksListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class NotebooksListViewController: UIViewController, UITableViewDataSource, NSFetchedResultsControllerDelegate {
+class NotebooksListViewController: UIViewController, UITableViewDataSource {
     /// A table view that displays a list of notebooks
     @IBOutlet weak var tableView: UITableView!
 
@@ -158,6 +158,32 @@ class NotebooksListViewController: UIViewController, UITableViewDataSource, NSFe
                 vc.notebook = fetchedResultsController.object(at: indexPath)
                 vc.dataController = dataController
             }
+        }
+    }
+}
+
+
+extension NotebooksListViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates() //start tableViewAnimations vs. tableView.reload()
+    }
+    
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()  //end tableViewAnimations vs. tableView.reload()
+    }
+    
+    
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+        case .insert:
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        default:
+            break
         }
     }
 }
